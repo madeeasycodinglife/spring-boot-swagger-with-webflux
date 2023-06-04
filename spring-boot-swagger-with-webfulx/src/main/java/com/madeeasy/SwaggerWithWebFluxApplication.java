25889523cf064d1138481e20adf515d506b3a589
Pabitra Bera <126568738+madeeasycodinglife@users.noreply.github.com>
2023-06-04 14:18:15 +0530

    Add files via upload

---
+++ spring-boot-swagger-with-webfulx/src/main/java/com/madeeasy/SwaggerWithWebFluxApplication.java
@@ -0,0 +1,14 @@
+package com.madeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SwaggerWithWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerWithWebFluxApplication.class, args);
+	}
+
+}
---
+++ spring-boot-swagger-with-webfulx/src/main/java/com/madeeasy/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.madeeasy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
